8ac27c9566a5ed00ffd29422f12555e23916ed03
Martin <martinboglione97@gmail.com>
2023-02-15 21:56:26 -0300

    Added gifs with GIPHY API and changed folder structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-
-
-const url = 'https://api.giphy.com/v1/gifs/search?api_key=GZbFtMqmfJaKLrAjqo4fczj17AmGHt7V&q=messi&limit=10&offset=0&rating=g&lang=en'
+import ListOfGifs from "./components/ListOfGifs";
+import getGifs from "./services/getGifs";
 
 function App() {
-  const [gifs, setGifts] = useState[];
-
-  useEffect(function () {
-    setGifts(differentGifs);
-  }, []);
-
   return (
     <div className="App">
       <section className="App-content">
-        {gifs.map((singleGif) => (
-          <img src={singleGif} alt="messi" />
-        ))}
+        <ListOfGifs keyword="messi" />
       </section>
     </div>
   );
---
+++ src/components/ListOfGifs.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import Gif from "./Gif";
+import getGifs from "../services/getGifs";
+
+export default function ListOfGifs({ keyword }) {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(
+    function () {
+      getGifs({ keyword }).then((gifs) => setGifs(gifs));
+    },
+    [keyword]
+  );
+
+  return gifs.map(({ id, title, url }) => (
+    <Gif key={id} title={title} url={url} id={id} />
+  ));
+}
