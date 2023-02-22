6263bf875e3657d921743eaec9790a0b894a803f
Martin <martinboglione97@gmail.com>
2023-02-22 18:18:17 -0300

    Added loading state

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -5,14 +5,22 @@ import getGifs from "../services/getGifs";
 
 export default function ListOfGifs({ params }) {
   const { keyword } = params;
+  const [loading, setLoading] = useState(false);
+
   const [gifs, setGifs] = useState([]);
 
   useEffect(
     function () {
-      getGifs({ keyword }).then((gifs) => setGifs(gifs));
+      setLoading(true);
+      getGifs({ keyword }).then((gifs) => {
+        setGifs(gifs);
+        setLoading(false);
+      });
     },
     [keyword]
   );
+
+  if (loading) return <i>Loading</i>;
 
   return (
     <div className="container-gifs">
